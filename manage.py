1ceadf0bafa8086e464eae911906de671988ccf7
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-08 17:11:48 +0430

    برنامه ای برای تشخیص عدد زوج و فرد
    برنامه ای برای تشخیص عدد زوج و فرد.
    تا زمانی که کاربر بخواهد ، می تواند ادامه دهد. 
    اگر خواست این کار را به پایان برساند تایپ می کند e 
    Write a program that takes a number from the user. Then tell the user whether the number is even or odd
    A program for recognizing even and odd numbers.
    As long as the user wants, he can continue.
    Type e if he wants to finish it

---
+++ manage.py
@@ -0,0 +1,25 @@
+# برنامه ای بنویسید که عددی را از کاربر بگیرد. سپس به کاربر بگوید که آیا عدد زوج است یا فرد
+
+import os
+os.system('cls')
+
+input_number = input('write your number: ')
+print('\t')
+
+
+while input_number != 'e':    
+    if int(input_number) % 2 == 0:
+        print('Your number is Even')
+        print('\t')
+        print('if you want to Exit, write (e)')
+        print('\t')
+        input_number = input('write your number: ')
+    else:
+        print('Your number is Odd')
+        print('\t')
+        print('if you want to Exit, write (e)')
+        print('\t')
+        input_number = input('write your number: ')
+     
+print('\t')
+print('the Game is done.')
