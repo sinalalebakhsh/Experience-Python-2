9a9de138a1d1e5a5b878578034ba7fb25d62e176
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-14 13:37:49 +0430

    Add files via upload

---
+++ 20- def introduction ver2.py
@@ -0,0 +1,13 @@
+
+def import_os():
+    import os
+    os.system('cls')
+import_os()
+
+name_input_user = input('What\'s your name? ')
+
+def name_str(name_input_user):
+    print(f'Hi, Your name\'s {name_input_user}')
+
+
+name_str(name_input_user)
