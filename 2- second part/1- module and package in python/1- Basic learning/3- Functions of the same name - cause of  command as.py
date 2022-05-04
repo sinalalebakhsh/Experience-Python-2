349a403478c54d71e1bae7a6159a93552c165972
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-05 00:13:32 +0430

    Add files via upload

---
+++ 2- second part/1- module and package in python/1- Basic learning/3- Functions of the same name - cause of  command as.py
@@ -0,0 +1,12 @@
+from More_area import cleaner
+cleaner()
+
+from Main_area import closerpage as closepagefrom_Main_area
+
+from More_area import closerpage
+
+print()
+closerpage()
+print()
+closepagefrom_Main_area()
+print()
---
+++ 2- second part/1- module and package in python/1- Basic learning/More_area.py
@@ -0,0 +1,10 @@
+import os
+
+def cleaner():
+    return os.system('cls')
+
+def printer():
+    print('This is printer function.')
+
+def closerpage():
+    print('for example .... !!!!')
