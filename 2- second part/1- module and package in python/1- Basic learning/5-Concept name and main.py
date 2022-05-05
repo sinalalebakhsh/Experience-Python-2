d4d1da864f2f94b8151b547b8f55208fdc845087
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-05 19:01:48 +0430

    Add files via upload

---
+++ 2- second part/1- module and package in python/1- Basic learning/5-Concept name and main.py
@@ -0,0 +1,8 @@
+import Main_area
+
+# If you do not want the executable files to be executed in the one file
+#  that was imported from here, you must put them in a condition.
+# اگر میخوای فایلهای اجرایی ، داخل اون یکی فایل که از اینجا import  انجام 
+# داده ، اجرا نشه باید داخل یک شرط بریزیشون .
+
+
