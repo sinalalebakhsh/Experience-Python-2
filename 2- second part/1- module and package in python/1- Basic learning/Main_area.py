cf34c81a8e193b3b256bbfbb495a71aba8acc0d1
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-05 19:01:25 +0430

    Update Main_area.py

--- 2- second part/1- module and package in python/1- Basic learning/Main_area.py
+++ 2- second part/1- module and package in python/1- Basic learning/Main_area.py
@@ -9,3 +9,12 @@ def printer():
 def closerpage():
     print('The page closed.')
 
+    
+    
+# when you write this : 
+print('this is main area')
+# So inside that one file runs
+# but if write this :    
+if __name__ == '__main__':
+    print('this is main area  for more ... **** ')
+#this not runs in another files
