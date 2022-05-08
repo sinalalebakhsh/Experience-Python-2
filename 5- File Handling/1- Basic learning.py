019a3c0c4353fa6634279a22ba0885654e7a6aea
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-08 23:24:37 +0430

    Create 1- Basic learning.py

---
+++ 5- File Handling/1- Basic learning.py
@@ -0,0 +1,14 @@
+file = open('sina lalebakhsh.txt', 'a')
+file.write('''
+sina lalebakhsh
+in iran
+live
+and 
+have dearm
+to goes
+every entire wolrd.
+so 
+respect 
+for his dream.
+or
+be quiet.''')
