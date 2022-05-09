a887de2cafcea95fea7fe094616d5f21893617c7
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-09 12:24:28 +0430

    Add files via upload

---
+++ 5- File Handling/4- Basic learning.py
@@ -0,0 +1,14 @@
+file = open('sina lalebakhsh.txt', 'r')
+
+
+text_ = file.read()
+
+text_ = text_.split('\n')
+
+while True:
+    user_input = input('Enter name: ')
+    if user_input == 'finish':
+        break
+    text_.append(user_input)
+
+print(text_)
