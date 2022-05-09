3648e749d83fe57b2f25f4c73e87d973f3b21483
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-09 21:49:26 +0430

    Add files via upload

---
+++ 6- Build a graphical interface/3- Show text in window.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+window =  tk.Tk()
+
+text_sina = tk.Label(text='سینا لاله بخش')
+text_sina.pack()
+
+text_sina_Eng = tk.Label(text='SINA LALEBAKHSH')
+text_sina_Eng.pack()
+
+
+window.mainloop()
