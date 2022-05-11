aa9d256fb214419b90166b8121d14a8a50d06e8f
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-11 14:41:04 +0430

    Update and rename 1- first part/Addition without the use of variables to 2022/1- first part/Addition without the use of variables.py

---
+++ 2022/1- first part/Addition without the use of variables.py
@@ -0,0 +1,13 @@
+# Calculate the following statement without using a variable:
+# Multiply 2 by 10 to the sum of 15 and 3, and then multiply it by the result
+# Multiply the number 3 by 1000. You can use parentheses 
+# عبارت زیر رو بدون استفاده از متغیر محاسبه کنین:
+# عدد ۲ به توان ۱۰ را در مجموع دو عدد ۱۵ و ۳ - ضرب کنین و سپس نتیجه آن را با نتیجه
+# ضرب عدد ۳ در عدد ۱۰۰۰ جمع بکنین. )میتونین از پرانتزگذاری استفاده کنین (
+
+print(((2**10)*(-3+15))+(3*1000))
+
+====================================>>>>>>>
+
+result:
+15288
